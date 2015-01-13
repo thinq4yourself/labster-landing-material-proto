eb8591ca954ca1e8f5f049fd4c1840da2cc1e957
thinq4yourself <joel@thinq4yourself.com>
2015-01-13 17:44:01 +0000

    updated to the mail bot and ajax output

--- js/bearded_mail_bot.js
+++ js/bearded_mail_bot.js
@@ -8,47 +8,59 @@ $(function() {
         submitSuccess: function($form, event) {
             event.preventDefault(); // prevent default submit behaviour
             // get values from FORM
-            var name = $("input#name").val();
+            var fname = $("input#fname").val();
+            var lname = $("input#lname").val();
             var email = $("input#email").val();
-            var phone = $("input#phone").val();
             var message = $("textarea#message").val();
-            var firstName = name; // For Success/Failure Message
+            var u = "1d10f79fce4bc733309cc414e";
+            var id = "b645a67c79";
+            var firstName = fname; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
             if (firstName.indexOf(' ') >= 0) {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
-                type: "POST",
+                url: "//beardandfedora.us9.list-manage.com/subscribe/post-json?c=?",
+                type: "GET",
                 data: {
-                    name: name,
-                    phone: phone,
-                    email: email,
-                    message: message
+                    u: u,
+                    id: id,
+                    EMAIL: email,
+                    FNAME: fname,
+                    LNAME: lname,
+                    MESSAGE: message
                 },
                 cache: false,
-                success: function() {
-                    // Success message
-                    $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-success')
-                        .append("<strong>Your beautiful message has been sent. </strong>");
-                    $('#success > .alert-success')
-                        .append('</div>');
-
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
+                dataType: 'json',
+                contentType: "application/json; charset=utf-8",
+                success: function(data) {
+                    if (data.result != "success") {
+                        // Fail message
+                        $('#success').html("<div class='alert alert-danger'>");
+                        $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                            .append("</button>");
+                        $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that our bearded mail robot had an issue. Could you kindly try again? <i>Pssst...Here's what happened: <span style='color: #000;'>" + data.msg + "</span></i>");
+                        $('#success > .alert-danger').append('</div>');
+                     } else {
+                        // Success message
+                        $('#success').html("<div class='alert alert-success'>");
+                        $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                            .append("</button>");
+                        $('#success > .alert-success')
+                            .append("<strong>Thank you " + firstName + ", your beautiful message is on its way to us right now. </strong>");
+                        $('#success > .alert-success')
+                            .append('</div>');
+                        //clear all fields
+                        $('#contactForm').trigger("reset");
+                    }
                 },
-                error: function() {
+                error: function(err) {
                     // Fail message
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that our bearded mail robot is not responding. Coud you kindly try again, or later? Thanks.");
+                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that our bearded mail robot is not responding. Could you kindly try again? Thanks. - " + err.msg + "");
                     $('#success > .alert-danger').append('</div>');
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
                 },
             })
         },
@@ -65,6 +77,6 @@ $(function() {
 
 
 /*When clicking on Full hide fail/success boxes */
-$('#name').focus(function() {
+$('#fname').focus(function() {
     $('#success').html('');
 });
