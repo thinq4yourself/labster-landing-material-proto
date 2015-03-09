8b1ec7376b6aa9750360320b9c1fbff1816343b8
thinq4yourself <joel@thinq4yourself.com>
2015-03-09 17:59:25 +0000

    updating material design themeupdating material design theme

---
+++ js/tour.js
@@ -0,0 +1,60 @@
+// Start the tour
+var tour = new Tour({
+  steps: [
+  {
+    element: "#tour-1",
+    title: "Title of this step",
+    content: "Content of this step"
+  },
+  {
+    element: "#tour-2",
+    title: "Title of this step",
+    content: "Content of this step"
+  },
+  {
+    element: "#tour-3",
+    title: "Title of this step",
+    content: "Content of this step"
+  },
+  {
+    element: "#tour-4",
+    title: "Title of this step",
+    content: "Content of this step"
+  },
+  {
+    element: "#tour-5",
+    title: "Title of this step",
+    content: "Content of this step"
+  },
+  {
+    element: "#tour-6",
+    title: "Title of this step",
+    content: "Content of this step"
+  },
+  {
+    element: "#tour-7",
+    title: "Title of this step",
+    content: "Content of this step"
+  },
+  {
+    element: "#tour-8",
+    title: "Title of this step",
+    content: "Content of this step"
+  },
+  {
+    element: "#tour-9",
+    title: "Title of this step",
+    content: "Content of this step"
+  },
+  {
+    element: "#tour-10",
+    title: "Title of this step",
+    content: "Content of this step"
+  }
+]});
+
+// Initialize the tour
+tour.init();
+
+// Start the tour
+tour.start();
